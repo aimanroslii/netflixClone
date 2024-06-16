25c9d3e32a0d1a1b93720e774cab7f6c857e7719
Aiman Rosli <aimanrosli977@yahoo.com>
2024-06-16 23:23:15 +0800

    Completed building banner part1
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

---
+++ netflix-clone-app/src/Banner.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n -1) + '...' : string
+    }
+  return (
+    <header className='banner' style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://w0.peakpx.com/wallpaper/896/738/HD-wallpaper-black-banner.jpg")`,
+        backgroundPosition: "center center"
+    }}>
+        <div className='banner_contents'>
+            <h1 className='banner_title'>Movie Name</h1>
+            <div className='banner_buttons'>
+                <button className='banner_button'>Play</button>
+                <button className='banner_button'>My List</button>
+            </div>
+            <h1 className='banner_description'>{truncate(`This is a description
+                This is a description This is a description This is a description
+                This is a description This is a descriptionThis is a descriptionThis is a description
+                This is a descriptionThis is a descriptionThis is a descriptionThis is a description
+                This is a descriptionThis is a descriptionThis is a description
+                This is a descriptionThis is a descriptionThis is a descriptionThis is a description
+                This is a descriptionThis is a descriptionThis is a descriptionThis is a description
+                This is a descriptionThis is a descriptionThis is a description`, 150)}</h1>
+
+        </div>
+        <div className='banner-fadeBottom'/>
+      
+    </header>
+  )
+}
+
+export default Banner
--- netflix-clone-app/src/HomeScreen.js
+++ netflix-clone-app/src/HomeScreen.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './HomeScreen.css';
 import './Nav.js';
 import Nav from './Nav.js';
+import Banner from './Banner.js';
 
 function HomeScreen() {
   return (
     <div className='homeScreen'>
        <Nav />
+       <Banner />
 
        {/* Banner */}
 
